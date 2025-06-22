c22c990d9a8aeb42d530c898400fb948a0db1675
LaraSouzx <larasouzadev@gmail.com>
2025-06-21 23:06:14 -0300

    fix: melhorias no hover dos icones

--- src/components/promocao/CartaoAcao.jsx
+++ src/components/promocao/CartaoAcao.jsx
@@ -8,13 +8,13 @@ const CartaoAcao = () => {
                     <h2 className="font-bold text-lg text-gray-90">Custom your Home Screen</h2>
                      <p className="text-sm text-gray-500">Change home widget</p>                
                 </div>
-                <button className="bg-[#30CFF2] text-white font-semibold rounded-full px-5 py-2  hover:bg-gray-900 hover:ring-2 hover:ring-gray-300 hover:ring-opacity-50 transition-all duration-300 transform hover:scale-105">
+                <button className="bg-gradient-to-b from-[#0aa9dc] to-[#5cc6e8] text-white font-semibold rounded-full px-5 py-2  hover:bg-gray-900 hover:ring-2 hover:ring-gray-300 hover:ring-opacity-50 transition-all duration-300 transform hover:scale-105">
                     Try Now
                 </button>
             </div>
 
             <div>
-                <IconeWidget className="w-16 h-16 text-gray-800" />
+                <IconeWidget className="w-20 h-20 text-gray-800" />
             </div>
         </div>
     );
--- src/components/promocao/IconeWidget.jsx
+++ src/components/promocao/IconeWidget.jsx
@@ -5,19 +5,17 @@ const IconeWidget = ({ className }) => {
     <svg 
       className={className} 
       viewBox="0 0 24 24" 
-      fill="none" 
-      stroke="currentColor" 
-      strokeWidth="1.5" 
+      fill="none"  
       strokeLinecap="round" 
       strokeLinejoin="round" 
       xmlns="http://www.w3.org/2000/svg"
     >
       {/* Quadrado inferior esquerdo */}
-      <rect x="4" y="14" width="6" height="6" rx="1" fill="currentColor" />
+      <rect x="4" y="14" width="6" height="6" rx="1" fill="#9F22F2" />
       {/* Quadrado inferior direito */}
-      <rect x="14" y="14" width="6" height="6" rx="1" fill="currentColor" />
+      <rect x="14" y="14" width="6" height="6" rx="1" fill="#846CD9" />
       {/* Quadrado superior esquerdo */}
-      <rect x="4" y="4" width="6" height="6" rx="1" fill="currentColor" />
+      <rect x="4" y="4" width="6" height="6" rx="1" fill="#644BBF" />
       {/* Sinal de mais */}
       <path d="M17 6V10" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
       <path d="M15 8H19" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
